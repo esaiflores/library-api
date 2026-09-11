5d142f124e6c88e9b5c783d391b31724f3a7549d
Esai Flores <flores.esaijonathan@gmail.com>
2026-09-11 15:01:10 -0500

    added vercel to security

--- src/main/java/com/esai/library_api/config/SecurityConfig.java
+++ src/main/java/com/esai/library_api/config/SecurityConfig.java
@@ -72,7 +72,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "https://library-management-web-rouge.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
